b7611447f93d8cd7f804449f8680bcb4c6ade64b
Anurag Arwalkar <anuragarwalkar@gmail.com>
2019-03-08 17:15:28 +0530

    created new project generated components generated services

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { WeatherDetailsComponent } from './components/weather-details/weather-details.component';
+import { WeatherComponent } from './components/weather/weather.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    WeatherDetailsComponent,
+    WeatherComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
